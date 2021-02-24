e654ade5ae892aa8f50fc78496eeb63f654e7621
Dil3mm4 <dil3mm4.dev@gmail.com>
2021-02-24 12:19:07 +0000

    Remove Lineage Touch

--- msm8953.mk
+++ msm8953.mk
@@ -386,9 +386,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_8953
 
 # Trust HAL
 PRODUCT_PACKAGES += \
